9944a349e9f2aed29f568c1e6ad6dbdad685584f
neerajvashistha <neerajvash8@gmail.com>
2016-03-28 17:54:30 +0530

    pdf add code mod

--- B4/Plagarism.java
+++ B4/Plagarism.java
@@ -24,7 +24,7 @@ public class Plagarism extends HttpServlet
             String fileTwo = request.getParameter("File2");
             out.println("Comparing the 2 files......");
             out.println("The result of the 2 files is ....");
-            if (fileOne.equals(fileTwo)) {
+            if (compareStrings(fileOne,fileTwo)) {
                 out.println("Plagiarism detected. Cheaters!!!!");
             } else {
                 out.println("No plagiarism detected");
@@ -36,6 +36,36 @@ public class Plagarism extends HttpServlet
             out.close();
         }
     }
+
+    public static boolean compareStrings(String a,String b){
+        boolean checkForPlagiarism = false;
+        String[] piecesA = a.split("\\s");
+        String[] piecesB = b.split("\\s");
+
+        int count1 = 0;
+        int count2 = 0;
+   
+        for (int counter = 0; counter <= piecesA.length - 1; counter++){
+            for(int counter2 = 0; counter2<= piecesB.length - 1; counter2++){
+                if(piecesA[counter].equals(piecesB[counter2])){
+                count1++;
+                }
+            }
+        }
+
+        for(int counter=0;counter<=piecesA.length-1;counter++){
+            for(int counter2 = 0; counter2 < piecesB.length; counter2++){
+                if(piecesA[counter].equals(piecesB[counter2])){
+                    count2++;
+                }
+            }
+        }
+   
+        if((count1/(int)piecesA.length)*100>=65 && (count2/(int)piecesB.length)*100>=65){
+            checkForPlagiarism=true;
+            }    
+        return checkForPlagiarism;
+        }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     @Override
