ec50c2c7c2a86922469b14169799cb6fb2b51830
neerajvashistha <neerajvash8@gmail.com>
2016-04-19 01:00:41 +0530

    A6 exception caught

--- Cl-3 as/A6/verifyPass.py
+++ Cl-3 as/A6/verifyPass.py
@@ -16,20 +16,24 @@ else:
         print('using default password: password')
         password = b'password'
 
-    #password = bytes(passwd,'UTF-8')
-    salT_file = open(username+"_salt.key")
-    chef_salt = salT_file.read()
-    print("Salt\n"+chef_salt)
-    dk = hashlib.pbkdf2_hmac(hash_name, password, bytes(chef_salt,'UTF-8'), 100000,128)
-    hashk = binascii.hexlify(dk)
-    new_hash = hashk.decode('utf-8')
-    hash_file = open(username+"_hash.hash")
-    old_hash  = hash_file.read()
-    print("hash\n"+old_hash)
-    if new_hash == old_hash:
-    	print("The user "+username+" has used valid password")
-    else:
-    	print("Incorrect username or password")
+    
+    try:#password = bytes(passwd,'UTF-8')
+        salT_file = open(username+"_salt.key")
+        chef_salt = salT_file.read()
+        print("Salt\n"+chef_salt)
+        dk = hashlib.pbkdf2_hmac(hash_name, password, bytes(chef_salt,'UTF-8'), 100000,128)
+        hashk = binascii.hexlify(dk)
+        new_hash = hashk.decode('utf-8')
+        hash_file = open(username+"_hash.hash")
+        old_hash  = hash_file.read()
+        print("hash\n"+old_hash)
+        if new_hash == old_hash:
+            print("The user "+username+" has used valid password")
+        else:
+            print("Incorrect username or password")
+    except OSError as e:
+        print("Incorrect username or password")
+    
 
 '''
 envy@envy-vm:~/be-2$ python3 encryptPass.py
